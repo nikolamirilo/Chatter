61c04322e0980bded3e9dc1b8a48d6145b820b54
Nikola Mirilo <nikolamirilo@gmail.com>
2021-11-14 12:05:53 +0100

    changed message form

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,7 +5,7 @@ import { auth } from "../firebase";
 import { useAuth } from "../contexts/AuthContext";
 import axios from "axios";
 import Loader from "react-loader-spinner";
-import ChatFeed from "./ChatFeed";
+// import ChatFeed from "./ChatFeed";
 
 const Chats = () => {
   const history = useHistory();
@@ -85,11 +85,11 @@ const Chats = () => {
         </div>
 
         <ChatEngine
-          height="86vh"
+          height="92vh"
           projectID="53835cc0-532c-4c62-a7eb-678f1dc0108f"
           userName={user.email}
           userSecret={user.uid}
-          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+          // renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
       </div>
     );
